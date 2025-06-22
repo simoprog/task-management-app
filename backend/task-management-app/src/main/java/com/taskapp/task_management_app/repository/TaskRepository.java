b2af40c2eb98ff4c31eeb23ca61734fef4c53ca7
Mohamed El Arfaoui <simoelarfaoui5@gmail.com>
2025-06-22 19:25:00 +0100

    fix liquibase changelog file

--- backend/task-management-app/src/main/java/com/taskapp/task_management_app/repository/TaskRepository.java
+++ backend/task-management-app/src/main/java/com/taskapp/task_management_app/repository/TaskRepository.java
@@ -6,11 +6,13 @@ import com.taskapp.task_management_app.enums.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task,Long> {
 
     List<Task> findByStatus(TaskStatus status);
