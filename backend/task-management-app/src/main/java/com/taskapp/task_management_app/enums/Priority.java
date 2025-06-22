cc7aed2dde6980e03be2e129805c9ac1c2aaf0ed
Mohamed El Arfaoui <simoelarfaoui5@gmail.com>
2025-06-22 16:30:11 +0100

    create task entity and enums

---
+++ backend/task-management-app/src/main/java/com/taskapp/task_management_app/enums/Priority.java
@@ -0,0 +1,7 @@
+package com.taskapp.task_management_app.enums;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ backend/task-management-app/src/main/java/com/taskapp/task_management_app/enums/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.taskapp.task_management_app.enums;
+
+public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    COMPLETED,
+}
